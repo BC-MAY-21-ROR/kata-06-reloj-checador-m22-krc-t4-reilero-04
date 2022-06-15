09ac748e4735face7ebfd8517c560ca614d97738
ruruisu <zaddozgl@gmail.com>
2022-06-15 17:11:41 -0500

    implemented logic for administrator user management
    Co-authored-by: @AgustinPalmaM <palma.agustin7@gmail.com>

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,16 @@
+class RegistrationsController < Devise::RegistrationsController
+    before_action :authenticate_employee!, :redirect_unless_admin,  only: [:new, :create]
+    skip_before_action :require_no_authentication
+  
+    private
+    def redirect_unless_admin
+      unless current_employee.try(:admin?)
+        flash[:error] = "Only admins can do that"
+        redirect_to root_path
+      end
+    end
+  
+    def sign_up(resource_name, resource)
+      true
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@
 
 Rails.application.routes.draw do
   # devise_for :users
-  devise_for :employees
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :employees, :controllers => { :registrations => 'registrations'}
   root 'attendances#index'
   # Defines the root path route ("/")
   # root "articles#index"
