ab68a4123dabb10fb16bd6b7aae78f8134008779
OwenLobato <owenlobatov@gmail.com>
2022-06-18 09:08:31 -0500

    Add average checks by month controller and view Need to code the logic
    Co-authored-by: AgustinPalmaM <palma.agustin7@gmail.com>
    Co-authored-by: @V-Juarez <mytacam78@gmail.com>
    Co-authored-by: @OwenLobato <owenlobatov@gmail.com>
    Co-authored-by: @ruruisu <zaddozgl@gmail.com>

--- app/controllers/checks_average_by_month_controller.rb
+++ app/controllers/checks_average_by_month_controller.rb
@@ -1,10 +1,11 @@
-class AttendancesByDayController < ApplicationController
+class ChecksAverageByMonthController < ApplicationController
   
   def index
-    @attendances_by_day = Attendance.all
+    @checks_average_by_month = Attendance.all
     @employees = Employee.all
     if params[:query_text].present?
       @employees = @employees.global_search(params[:query_text])
     end
   end
+  
 end
