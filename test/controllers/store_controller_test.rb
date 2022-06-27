e6e3a0005185194b5233a2ee878f46465d09936c
V-Juarez <mytacam78@gmail.com>
2022-06-27 13:30:57 -0600

    test(new) file store_controller_test that tests the store_controller Co-authored-by: @OwenLobato <owenlobato@gmail.com> Co-authored-by: @ruruisu <zaddozgl@gmail.com> Co-authored-by: @AgustinPalmaM <palma.agustin7@gmail.com>

---
+++ test/controllers/store_controller_test.rb
@@ -0,0 +1,55 @@
+require 'test_helper'
+
+class StoresControllerTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @admin = employees(:employee_one)
+    @not_admin = employees(:employee_two)
+  end
+
+  test 'should get index' do
+    sign_in @admin
+    get stores_path
+
+    assert_response :success
+  end
+
+  test "should get new" do
+    sign_in @admin
+    get new_store_url
+
+    assert_response :success
+  end
+
+  test "should create store" do
+    sign_in @admin
+    get new_store_path
+
+    assert_response :success
+  end
+
+  test 'allows to update store' do
+    sign_in @admin
+
+    patch store_path(stores(:store_one)), 
+      params: { 
+        store: {
+          name: 'Tesla',
+          address: 'Berlin',
+        }
+      }
+
+    assert_redirected_to stores_path
+    assert_equal flash[:notice], 'Store edited ok'
+  end
+
+  test "should destroy a store" do
+    sign_in @admin
+    assert_difference("Store.count", -1) do
+      delete stores_url(stores(:store_one))
+    end
+
+    assert_redirected_to stores_path
+    assert_equal flash[:notice], 'Store deleted ok'
+  end
+end
